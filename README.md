80ad01e2561d8cc098f73c053528e9805da4eb62
Alireza <alirezasaadat056@gmail.com>
2024-08-29 20:38:56 -0700

    README update

--- README.md
+++ README.md
@@ -1,21 +1,39 @@
-This project solves the knight problem and uses pygame for gui
+# Knight's tour
 
-What is the knight problem :
+This project shows the famous knight's tour in chess .
 
-The Knight's Tour problem is a classic problem in the field of computational mathematics and computer science. It is a puzzle where a chess knight is placed on an empty chess board and the goal is to move the knight to every square on the board exactly once without re-visiting any squares . (google)
+## What is a knight's tour
 
+A knight's tour is a sequence of moves of a knight on a chessboard such that the knight visits every square exactly once. If the knight ends on a square that is one knight's move from the beginning square (so that it could tour the board again immediately, following the same path), the tour is closed ; otherwise, it is open .
 
-At first you should choose the beginning block
+check out https://en.wikipedia.org/wiki/Knight%27s_tour for more information.
 
+### Some variatioins
 
-here is a demo :
+![Knights-Tour-Animation](https://github.com/user-attachments/assets/c79f1e63-ac35-4415-9405-37bad0972337) ![Knight's_tour_anim_2](https://github.com/user-attachments/assets/81f909aa-1e81-4809-a5c4-245b18c9b8a3)
 
+# This project
+
+visualize the Knight's tour from any square in an chessboard
+
+## dependencies
+
+```
+pygame
+```
+
+### Installing
+
+After installing python , run the following command in your terminal
+
+```
+pip install pygame
+```
+
+## Pictures
 
 ![Screenshot 2024-05-11 035828](https://github.com/AlirezaSaadatmand/Knight-Problem/assets/157215281/7c0ae124-2ba1-448e-87b9-e8aadbbb8ec6)
 
-
-
-it can also be big or small but you have to change the code yourself :) 
-
+it can also be big or small but you have to change the code yourself :)
 
 ![Screenshot 2024-05-11 035942](https://github.com/AlirezaSaadatmand/Knight-Problem/assets/157215281/19c1d882-0a4c-4e05-bbe1-f5d1137e7195)
--- knight problem.py
+++ knight problem.py
@@ -3,8 +3,8 @@ from sys import exit
 import pyautogui
 
 size = pyautogui.size()
-WIDTH , HEIGHT = size
-WIDTH , HEIGHT = (WIDTH*9//10) , (HEIGHT*9//10)
+WIDTH, HEIGHT = size
+WIDTH, HEIGHT = (WIDTH*9//10), (HEIGHT*9//10)
 
 UNIT = 80
 
@@ -16,16 +16,17 @@ knight_position = None
 
 choosing = True
 
+
 class Block:
-    def __init__(self , xpos , ypos , x , y , id , color):
-        
+    def __init__(self, xpos, ypos, x, y, id, color):
+
         self.xpos = xpos
         self.ypos = ypos
-        
+
         self.x = x
         self.y = y
         self.checked = False
-        
+
         self.neighbors = []
 
         self.id = id
@@ -35,28 +36,27 @@ class Block:
             self.text_color = "black"
         else:
             self.text_color = "white"
-            
-        self.sur = pygame.Surface( (UNIT , UNIT) )
-        self.sur_rect = self.sur.get_rect(topleft = (self.x , self.y))
-        
-    
+
+        self.sur = pygame.Surface((UNIT, UNIT))
+        self.sur_rect = self.sur.get_rect(topleft=(self.x, self.y))
+
     def get_neighbor(self):
         self.neighbors = []
         x = self.xpos
         y = self.ypos
-        topLeft = [x-1 , y -2]
-        topRight = [x+1 , y -2]
-        buttomLeft = [x-1 , y +2]
-        buttomRight = [x+1 , y +2]
-        leftTop = [x-2 , y -1]
-        leftButtom = [x-2 , y +1]
-        rightTop = [x+2 , y -1]
-        rightButtom = [x+2 , y +1]
-        
+        topLeft = [x-1, y - 2]
+        topRight = [x+1, y - 2]
+        buttomLeft = [x-1, y + 2]
+        buttomRight = [x+1, y + 2]
+        leftTop = [x-2, y - 1]
+        leftButtom = [x-2, y + 1]
+        rightTop = [x+2, y - 1]
+        rightButtom = [x+2, y + 1]
+
         for block in blocks:
-            if [block.xpos , block.ypos] in [topLeft , topRight , buttomLeft , buttomRight , leftTop , leftButtom , rightTop , rightButtom] and block.checked == False:
+            if [block.xpos, block.ypos] in [topLeft, topRight, buttomLeft, buttomRight, leftTop, leftButtom, rightTop, rightButtom] and block.checked == False:
                 self.neighbors.append(block)
-        
+
     def move(self):
         lst = []
         for n in self.neighbors:
@@ -68,15 +68,15 @@ class Block:
             return self.neighbors[r].id
         else:
             return False
-        
+
     def draw(self):
-        if self.checked :
+        if self.checked:
             self.color = "yellow"
 
         self.sur.fill(self.color)
-        screen.blit(self.sur , self.sur_rect)
-        pygame.draw.rect(screen , "black", self.sur_rect,  1, 0)
-        
+        screen.blit(self.sur, self.sur_rect)
+        pygame.draw.rect(screen, "black", self.sur_rect,  1, 0)
+
 
 def create_board():
 
@@ -86,7 +86,7 @@ def create_board():
     for i in range(count_block):
         x = (WIDTH / 2) - (UNIT * count_block / 2)
         for j in range(count_block):
-            blocks.append(Block( j , i , x , y , id , color))
+            blocks.append(Block(j, i, x, y, id, color))
             if id % count_block != 0:
                 if color == "white":
                     color = "black"
@@ -95,15 +95,18 @@ def create_board():
             x += UNIT
             id += 1
         y += UNIT
-    
+
+
 create_board()
+
 
 def move(knight_position):
 
     for block in blocks:
         if block.id == knight_position:
             block.get_neighbor()
-            return block.move()          
+            return block.move()
+
 
 def get_start(pos):
     for block in blocks:
@@ -111,7 +114,7 @@ def get_start(pos):
             block.color = "yellow"
         else:
             block.color = block.cover_color
-            
+
 
 def draw(screen):
     screen.fill("#f59563")
@@ -120,7 +123,7 @@ def draw(screen):
 
 
 pygame.init()
-screen = pygame.display.set_mode( (WIDTH , HEIGHT) )
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 screen.fill("#f59563")
 pygame.display.set_caption("KNIGHT PROBLEM")
 clock = pygame.time.Clock()
@@ -133,7 +136,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.KEYDOWN: 
+        if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 exit()
@@ -149,8 +152,8 @@ while True:
         pygame.display.update()
         clock.tick(60)
     else:
-        if running:    
-            
+        if running:
+
             knight_position = move(knight_position)
             if knight_position == False:
                 for block in blocks:
@@ -160,7 +163,7 @@ while True:
             for block in blocks:
                 if block.id == knight_position:
                     block.checked = True
-                    
+
             draw(screen)
         pygame.display.update()
         clock.tick(5)
